9f95f89b0a17c14273efbec39c2978aeccbbabac
Alexis Moulliere <alexismoulliere@MacBook-Pro-de-Alexis.local>
2023-10-04 17:02:39 +0200

    finish

--- main.js
+++ main.js
@@ -5,20 +5,19 @@ let div = document.getElementById("d");
 
 function list_recipe (){
     const recipe = ["Pâtes","Oeuf","Sel","Poivre","Lardon","Oignon","Parmesan"].sort();
-    //let tri = recipe.sort();
-    console.log(recipe);
     for(i = 0; i < recipe.length; i++){
-        document.getElementsByClassName("ingredient" + (i+1)).textcontent = recipe[i];
+        console.log(recipe[i]);
+        document.getElementById("ingredient" + (i+1)).textContent = recipe[i];
+        
     }
     
 }
 
-function display_recipe(){
+togg.addEventListener("click", () => {
     if(getComputedStyle(div).display != "none"){
       div.style.display = "none";
     } else {
       div.style.display = "block";
     }
-  };
+  })
 list_recipe();
-togg.onclick = display_recipe();
