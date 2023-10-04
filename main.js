2cac6f3f7cd267c36655b20e1bdd43b91be18473
Alexis Moulliere <alexismoulliere@MBP-de-Alexis.lan>
2023-10-04 22:19:00 +0200

    finish2

--- main.js
+++ main.js
@@ -4,20 +4,19 @@ let div = document.getElementById("d");
 
 
 function list_recipe (){
-    const recipe = ["Pâtes","Oeuf","Sel","Poivre","Lardon","Oignon","Parmesan"].sort();
-    for(i = 0; i < recipe.length; i++){
-        console.log(recipe[i]);
-        document.getElementById("ingredient" + (i+1)).textContent = recipe[i];
+    const recipe = ["Pâtes","Oeuf","Sel","Poivre","Lardon","Oignon","Parmesan"].sort();//tri par ordre alphabétique avec .sort()
+    for(i = 0; i < recipe.length; i++){ //boucle sur le tableau
+        document.getElementById("ingredient" + (i+1)).textContent = recipe[i];//ajout des ingrédients dans la liste 
         
     }
     
 }
 
 togg.addEventListener("click", () => {
-    if(getComputedStyle(div).display != "none"){
+    if(getComputedStyle(div).display != "none"){// obtenir les styles de l'élément(div), ensuite on récupère la valeur de la propriété "display"
       div.style.display = "none";
     } else {
       div.style.display = "block";
     }
   })
-list_recipe();
+list_recipe();//appel de la function
